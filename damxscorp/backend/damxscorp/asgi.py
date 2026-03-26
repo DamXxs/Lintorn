3754c3e389d666e796e17c6cd2a6b18e6af5c631
DamXxs <95287261+DamXxs@users.noreply.github.com>
2026-03-26 12:45:28 +0000

    correctif backend !

--- damxscorp/backend/damxscorp/asgi.py
+++ damxscorp/backend/damxscorp/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empire_meca.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'damxscorp.settings')
 
 application = get_asgi_application()
